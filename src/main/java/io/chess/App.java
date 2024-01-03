58676f9e26ed28d20d2475470ebaf109f1ba6588
Siddharth VP <siddharthvp@gmail.com>
2024-01-03 13:09:10 +0530

    Add README

--- src/main/java/io/chess/App.java
+++ src/main/java/io/chess/App.java
@@ -6,11 +6,13 @@ public class App {
     public static void main(String[] args) {
         Board board = new Board();
         Scanner in = new Scanner(System.in);
+        System.out.print("White to play: ");
         while (in.hasNextLine()) {
             String move = in.nextLine();
             try {
                 board.move(move);
                 System.out.println(Visualisers.ascii(board));
+                System.out.print((board.isMoverColor() ? "White" : "Black") + " to play: ");
             } catch (Exception e) {
                 System.out.println("Failed to move " + move);
                 e.printStackTrace(System.out);
